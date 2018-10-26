de2e107677671f9ed3e2530b395f50e885cc9a66
qlyx <1099781379@qq.com>
2018-10-26 16:30:58 +0800

    ;

---
+++ ceshi/SSSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSSViewController.h
+//  111
+//
+//  Created by jiangxiao on 2018/10/26.
+//  Copyright © 2018年 江萧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSSViewController : UIViewController
+
+@end
---
+++ ceshi/SSSViewController.m
@@ -0,0 +1,38 @@
+
+//
+//  SSSViewController.m
+//  111
+//
+//  Created by jiangxiao on 2018/10/26.
+//  Copyright © 2018年 江萧. All rights reserved.
+//
+
+#import "SSSViewController.h"
+
+@interface SSSViewController ()
+
+@end
+
+@implementation SSSViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
